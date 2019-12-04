2f9b311ad91ad2a2161068d7e05383c7fad1153a
Tia Lim <limqiying19@gmail.com>
2019-12-04 00:59:55 -0500

    hitable class for objects that can be hit by a ray

---
+++ include/hitable.h
@@ -0,0 +1,18 @@
+#ifndef HITABLEH
+#define HITABLEH
+
+#include "ray.h"
+#include <Eigen/Dense>
+
+struct hit_record {
+    float t;
+    Eigen::Vector3f p;
+    Eigen::Vector3f normal;  
+};
+
+class hitable {
+    public: 
+    virtual bool hit(const ray& r, float t_min, float t_max, hit_record&rec) const = 0;
+};
+
+#endif /* HITABLEH */
