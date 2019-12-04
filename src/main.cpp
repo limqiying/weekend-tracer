fb871d04d6ebdb4a88bb246e989cf435f13ba4a1
Tia Lim <limqiying19@gmail.com>
2019-12-03 23:40:02 -0500

    added comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 #include <fstream>
 #include "ray.h"
+#include <Eigen/Dense>
 
 
 using namespace std; using namespace Eigen;
 
 Vector3f color(const ray& r) {
+    /*
+    * Linearly interpolates blue and white, depending on the
+    * y-direction value of the ray
+    */
     Vector3f unit_direction = r.direction().normalized();
     float t = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - t) * Vector3f::Ones() + t * Vector3f(0.5, 0.7, 1.0);
@@ -20,10 +25,10 @@ int main() {
     
     imageFile << "P3\n" << nx << " "<< ny << "\n255\n";
 
-    Vector3f lower_left_corner(-2.0, -1.0, -1.0);
-    Vector3f horizontal(4.0, 0.0, 0.0);
-    Vector3f vertical(0.0, 2.0, 0.0);
-    Vector3f origin(0.0, 0.0, 0.0);
+    Vector3f lower_left_corner(-2.0, -1.0, -1.0);   // the lower left hand cordiner of the 200 x 100 image
+    Vector3f horizontal(4.0, 0.0, 0.0); // the horizontal extents of the image
+    Vector3f vertical(0.0, 2.0, 0.0);   // the vertical extents of the image
+    Vector3f origin(0.0, 0.0, 0.0); // the origin of the camera location
     
     for (int j = ny-1; j >= 0; j--) {
         for (int i=0; i < nx; i++) {
@@ -31,6 +36,7 @@ int main() {
             float u = float(i) / float(nx);
             float v = float(j) / float(ny);
 
+            // the ray direction here is not normalized for simpler and faster code
             ray r(origin, lower_left_corner + u * horizontal + v * vertical);
             Vector3f col = color(r);
 
