68a3edf634f644a4749e76b35bfa9fe71d7e21c7
Tia Lim <limqiying19@gmail.com>
2019-12-04 23:31:30 -0500

    add class pointer to fix circular dependency

--- include/hitable.h
+++ include/hitable.h
@@ -4,17 +4,20 @@
 #include "ray.h"
 #include <Eigen/Dense>
 
+class material;
+
 struct hit_record 
 {
     float t;
     Eigen::Vector3f p;
     Eigen::Vector3f normal;  
+    material *mat_ptr;
 };
 
 class hitable 
 {
     public: 
-        virtual bool hit(const ray& r, float t_min, float t_max, hit_record&rec) const = 0;
+        virtual bool hit(const ray& r, float t_min, float t_max, hit_record& rec) const = 0;
 };
 
 #endif /* HITABLEH */
